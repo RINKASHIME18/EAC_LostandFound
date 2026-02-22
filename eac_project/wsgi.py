b8db7bdbe445e3bdb86b9e9bfade0d5fd171a0d3
Smrk <93406119+RINKASHIME18@users.noreply.github.com>
2026-02-23 03:24:57 +0800

    Rename application variable to app

--- eac_project/wsgi.py
+++ eac_project/wsgi.py
@@ -1,16 +1,1 @@
-"""
-WSGI config for eac_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eac_project.settings')
-
-application = get_wsgi_application()
+app = application
