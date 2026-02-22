5762d9966a8f67f118876553e3a88cde499c54db
RINKASHIME18 <Markahldenmendoza@gmail.com>
2026-02-23 04:57:40 +0800

    Improve build script: use virtual environment for dependencies

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Building project..."
-python3.12 -m pip install -r requirements.txt --break-system-packages
-export PYTHONPATH=$PYTHONPATH:.
-python3.12 manage.py collectstatic --noinput --clear
+python3.12 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
 echo "Build complete."
